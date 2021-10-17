001274f278d88e5f8b573720254a92f21006e177
Lucas Cardoso <lucasimwsc@gmail.com>
2021-10-16 23:57:55 -0300

    Criando a arvore

--- 1.py
+++ 1.py
@@ -1,2 +1,10 @@
-print("Hello world")
-print('teste')
+HEAP = [0, 100, 19, 36, 17, 3, 25, 1, 2, 7]
+
+#Funcao para ver se a arvore eh HEAP, slide do professor
+def eh_heap(A):
+  for i in range(2, len(A)):
+    if A[i] > A[i//2]:
+      return false
+  return true
+
+print(eh_heap(HEAP))
