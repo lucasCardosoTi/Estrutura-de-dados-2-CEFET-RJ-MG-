3c9acec51b2c2587498db0461874627d75b98799
Lucas Cardoso <lucasimwsc@gmail.com>
2021-10-17 21:39:50 -0300

    Mais um

--- 4_arvoreImagem.py
+++ 4_arvoreImagem.py
@@ -1,24 +1,32 @@
+# Função imagem de uma HEAP
 
+def nivel(i=0):
+    return 2**i
 
-A = [0, 100, 30, 10, 6, 8, 15, 18, 19]
-B = [0,1,2,3]
-C = [0,1,2,3,4,5,6,7]
+def heap_imagem(A):
+    total = 0
+    imagem = A.copy()
+    i = 1
+    while total < len(A)//2:
+        n = nivel(i)
+        for k in range(0,n):
+            imagem[n+k] = A[2*n + (-1)*k - 1]
+        total = total + n
+        i = i + 1
+    return imagem
 
-def arvoreImagem(A):
-    img = []
-    img.append(A[0])
-    img.append(A[1])
-    for i in range (1, len(A)):
-        if(i*2+1 <= len(A) - 1):    
-            img.append(A[i*2+1])
-        if(i*2 <= len(A) - 1):
-            img.append(A[i*2])
-    
-    return img
+HEAP = [0, 1, 2, 3]
+HEAP_imagem = heap_imagem(HEAP)
+print("HEAP  : ",HEAP)
+print("IMAGEM: ",HEAP_imagem)
 
-print("Arvore B:")
-print(B)
-print(arvoreImagem(B))
-print("Arvore C:")
-print(C)
-print(arvoreImagem(C))
+HEAP = [0, 1, 2, 3, 4, 5, 6, 7]
+HEAP_imagem = heap_imagem(HEAP)
+print("HEAP  : ",HEAP)
+print("IMAGEM: ",HEAP_imagem)
+
+HEAP = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,
+        18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+HEAP_imagem = heap_imagem(HEAP)
+print("HEAP  : ",HEAP)
+print("IMAGEM: ",HEAP_imagem)
